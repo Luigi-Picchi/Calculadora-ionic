c4719993025144a766b6d05b207a2d35459358a2
Lucas Andreatta <drea.pierra@gmail.com>
2023-11-18 19:24:09 -0300

    refatorado, adição de potenciação e possibilidade de seguir calculando com o resultado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   resultado: string = '0';
-  checa_operador: boolean = false;
   primeiro_elemento: string = '';
   segundo_elemento: string = '';
   operador_selecionado: string = '';
@@ -16,68 +15,77 @@ export class HomePage {
   memoria: string = '';
   resultado_concluido: boolean = false;
 
-  constructor() {}
+  constructor() { }
 
-  digito(valor: string){
-    if(this.resultado_concluido){
+  // pega a entrada de digitos e faz algumas validações
+  digito(valor: string) {
+    // se o resultado já foi calculado e o usuário digitou um numero, limpa a tela e seta o resultado como primeiro elemento
+    if (this.resultado_concluido && !this.comeca_segundo_elemento) {
       this.resultado = valor;
       this.resultado_concluido = false;
-      this.checa_operador = false;
       this.segundo_elemento = '';
-    }else{
-    if(this.comeca_segundo_elemento) {
-      this.segundo_elemento = this.segundo_elemento + valor;
-      this.resultado =this.resultado + valor;
-    }else{
-    if(this.resultado =='0'){
-      this.resultado = valor;
-    } else{
-    this.resultado = this.resultado + valor;
-  }
-}
-  }
-}
-
-operador(valor: string){
-  if(!this.checa_operador){
-    this.primeiro_elemento = this.resultado;
-  this.resultado += valor;
-  this.checa_operador = true;
-  this.comeca_segundo_elemento = true;
-  this.operador_selecionado = valor;
-  }
-}
-
-redefinir(){
-  this.resultado = "0";
-  this.checa_operador = false;
-  this.primeiro_elemento = '';
-  this.segundo_elemento = '';
-  this.operador_selecionado = '';
-  this.comeca_segundo_elemento = false;
-  this.memoria = '';
-}
-
-calcular(){
-  if(this.operador_selecionado == "+" && this.segundo_elemento != ""){
-    this.resultado = (parseInt(this.primeiro_elemento) + parseInt(this.segundo_elemento)).toString();
-    this.memoria = this.primeiro_elemento + this.operador_selecionado + this.segundo_elemento + "=" + this.resultado;
-    this.resultado_concluido = true;
-  }else if(this.operador_selecionado == "-" && this.segundo_elemento != ""){
-    this.resultado = (parseInt(this.primeiro_elemento) - parseInt(this.segundo_elemento)).toString();
-    this.memoria = this.primeiro_elemento + this.operador_selecionado + this.segundo_elemento + "=" + this.resultado;
-    this.resultado_concluido = true;
-  }else if(this.operador_selecionado == "*" && this.segundo_elemento != ""){
-    this.resultado = (parseInt(this.primeiro_elemento) * parseInt(this.segundo_elemento)).toString();
-    this.memoria = this.primeiro_elemento + this.operador_selecionado + this.segundo_elemento + "=" + this.resultado;
-    this.resultado_concluido = true;
-  }else if (this.operador_selecionado == "/" && this.segundo_elemento != ""){
-    this.resultado = (parseInt(this.primeiro_elemento) / parseInt(this.segundo_elemento)).toString();
-    this.memoria = this.primeiro_elemento + this.operador_selecionado + this.segundo_elemento + "=" + this.resultado;
-    this.resultado_concluido = true;
-  }
+    } else {
+      this.resultado = this.comeca_segundo_elemento ? this.resultado + valor : (this.resultado == '0' ? valor : this.resultado + valor);
+      if (this.comeca_segundo_elemento) {
+        this.segundo_elemento += valor;
+      }
     }
   }
 
+  // processa entrada de operadores e setar o estado de algumas variaveis
+  operador(valor: string) {
+    if (!this.comeca_segundo_elemento) {
+      this.primeiro_elemento = this.resultado;
+      this.resultado += valor;
+      this.comeca_segundo_elemento = true;
+      this.operador_selecionado = valor;
+      this.resultado_concluido = false;
+    }
+  }
 
+  // limpa a tela e as variáveis
+  redefinir() {
+    this.resultado = "0";
+    this.primeiro_elemento = '';
+    this.segundo_elemento = '';
+    this.operador_selecionado = '';
+    this.comeca_segundo_elemento = false;
+    this.memoria = '';
+  }
 
+  // calcula e converte os tipos
+  calcular() {
+    if (this.segundo_elemento != "") {
+      switch (this.operador_selecionado) {
+        case '+':
+          this.resultado = (parseInt(this.primeiro_elemento) + parseInt(this.segundo_elemento)).toString();
+          break;
+        case '-':
+          this.resultado = (parseInt(this.primeiro_elemento) - parseInt(this.segundo_elemento)).toString();
+          break;
+        case '*':
+          this.resultado = (parseInt(this.primeiro_elemento) * parseInt(this.segundo_elemento)).toString();
+          break;
+        case '/':
+          if (this.segundo_elemento == '0') {
+            this.resultado = 'Erro: divisão por zero';
+            return;
+          } else {
+            this.resultado = (parseInt(this.primeiro_elemento) / parseInt(this.segundo_elemento)).toString();
+          }
+          break;
+        case '^':
+          this.resultado = Math.pow(parseInt(this.primeiro_elemento), parseInt(this.segundo_elemento)).toString();
+          break;
+      }
+      this.memoria = this.primeiro_elemento + this.operador_selecionado + this.segundo_elemento + "=" + this.resultado;
+      this.resultado_concluido = true;
+
+      // seta as variaveis pra poder utilizar o resultado como primeiro elemento e continuar o calculo
+      this.primeiro_elemento = this.resultado;
+      this.segundo_elemento = '';
+      this.operador_selecionado = '';
+      this.comeca_segundo_elemento = false;
+    }
+  }
+}
